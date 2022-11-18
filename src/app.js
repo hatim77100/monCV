f213e1dd7bf94387443246a03786616a56009518
hatim77100 <najoua-hatim@hotmail.fr>
2022-11-18 16:57:38 +0100

    update desktop

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
 import "./assets/css/styles.css";
 import "@fortawesome/fontawesome-free/js/all";
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
